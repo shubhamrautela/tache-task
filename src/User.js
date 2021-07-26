72bee618ad9f9a8f25eb434a1b9aad0c7c23a555
shubham rautela <rautelashubham@gmail.com>
2021-07-26 05:48:58 +0530

    changes

--- src/User.js
+++ src/User.js
@@ -1,6 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import {useSelector} from 'react-redux'
 import {Link} from 'react-router-dom'
+import GoogleMapReact from 'google-map-react';
+
+
+const AnyReactComponent = ({ text }) => <div className="marker">{text}</div>;
 
 const User = ({match}) => {
     const logged = useSelector((state) => state.isLoggedIn)
@@ -10,6 +14,17 @@ const User = ({match}) => {
     const albums = useSelector((state) => state.data[1])
     const photos = useSelector((state) => state.data[2])
     
+    const coord = userData && userData.address && [+userData.address.geo.lat, +userData.address.geo.lng]
+    const defaultProps = {
+        center: {
+          lat: coord && coord[0],
+          lng: coord && coord[1]
+        },
+        zoom: 11
+      };
+
+
+
     const finalAlbum= []
     useEffect(() => {
         const usersData = users && users.filter((person)=> user === person.username)
@@ -56,6 +71,23 @@ logged.isLoggedIn ?
                 
   
             </div>
+            <div id="mapid">
+            <div style={{ height: '250px', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+           lat={ coord && coord[0]}
+           lng={coord && coord[1]}
+          text="."
+        />
+      </GoogleMapReact>
+    </div>
+
+            </div>
+            
             
             <div className="albums">
                {
